7431a95097a4a80b3a2bb21a1b9ca9a45da1f6e5
JGaldp-beep <juan.galdo@ucb.edu.bo>
2025-04-23 23:13:57 -0400

    Base code

--- app/src/main/java/com/example/movieapp/MovieAppScreen.kt
+++ app/src/main/java/com/example/movieapp/MovieAppScreen.kt
@@ -17,8 +17,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.example.movieapp.ui.MovieList
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
@@ -31,7 +29,7 @@ fun MovieApp() {
                 .fillMaxSize()
                 .padding(innerPadding)
         ) {
-
+            /* Todo: Implement Navigation */
         }
     }
 }
